09a88d40cfd9677a7dc4b0f9b0b4d0110d928310
Yuki326 <88820769+Yuki326@users.noreply.github.com>
2021-10-02 15:25:10 +0900

    シャッフル機能の追加

--- src/Start.js
+++ src/Start.js
@@ -1,34 +1,39 @@
 import React, { Component } from "react";
 let card = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
+let ans = [0, 1, 2, 3];
 function shuffle() {
   var a = 0;
   var b = 0;
   var tmp = 0;
-  a = Math.floor(Math.random() * 10);
-  b = Math.floor(Math.random() * 10);
-  tmp = card[a];
-  card[a] = card[b];
-  card[b] = tmp;
+  for (var i = 0; i <= 10; i++) {
+    a = Math.floor(Math.random() * 10);
+    b = Math.floor(Math.random() * 10);
+    tmp = card[a];
+    card[a] = card[b];
+    card[b] = tmp;
+  }
+  for (var j = 0; j <= 3; j++) {
+    ans[j] = card[j];
+  }
 }
 class Start extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: card
+      data: ans
     };
   }
   shuffle = () => {
     shuffle();
     this.setState({
-      data: card
+      data: ans
     });
   };
   render() {
     return (
       <>
         <h1>{this.state.data}</h1>
-        <button onClick={this.shuffle}>Start</button>
+        <button onClick={this.shuffle}>シャッフル</button>
       </>
     );
   }
