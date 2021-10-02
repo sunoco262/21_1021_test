73aac3a9b42d66da3f7dd8022c40c8eaaa36284f
sunoco262 <91724015+sunoco262@users.noreply.github.com>
2021-10-02 12:50:41 +0900

    change text wakuwaku

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactテスト
+          Learn React wakuwakutintin
         </a>
       </header>
     </div>
