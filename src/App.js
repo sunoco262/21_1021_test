d9c2b9423b49ad219455f0d15d3b35c96d6291e3
sunoco262 <91724015+sunoco262@users.noreply.github.com>
2021-10-03 12:25:30 +0900

    Counterの修正

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React, { Component } from "react";
 import "./App.css";
 import Numeron from "./Numeron.js";
 import Timer from "./Timer.js";
-import Counter from "./Counter.js";
 class App extends Component {
   render() {
     return (
       <>
         <Numeron />
         <Timer />
-        <Counter />
       </>
     );
   }
