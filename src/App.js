b22ab0897c5b0b635b4b827080d48f071e91ecc7
Yuki326 <s20a1035@gmail.com>
2022-02-04 12:02:40 +0900

    履歴を表示するための枠を追加

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 import Numeron from "./Numeron.js";
 import Timer from "./Timer.js";
+import History from "./History.js";
 class App extends Component {
   render() {
     return (
       <div className="body">
         <Numeron />
         <Timer/>
+        <History/>
       </div>
     );
   }
