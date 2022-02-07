d096b7d23954624124b45c075ae5a976bd582072
Yuki326 <s20a1035@gmail.com>
2022-02-07 17:20:46 +0900

    commit

--- src/Form.js
+++ src/Form.js
@@ -43,7 +43,7 @@ function check(ans, input) {
         bite++;
       }
     }
-    str = eat + "eat　" + bite + "bite";
+    str = eat + "eat " + bite + "bite";
   }
   history.push(input+" "+str)
   if(eat===4){
